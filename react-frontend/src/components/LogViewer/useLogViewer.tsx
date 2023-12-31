df19cbe6b72d8c77d9f2feffef85ce8fe2e284b2
Nicolae Filat <nicolae.filat@gmail.com>
2023-12-31 19:04:17 +0100

    Make vite forward sockets to server directly

--- react-frontend/src/components/LogViewer/useLogViewer.tsx
+++ react-frontend/src/components/LogViewer/useLogViewer.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {user} from "../../utility/user.ts";
-import {API_HTTP_URL, socketConnection, socketMessages} from "../../utility/socketConnection.ts";
+import {apiFetch, socketConnection, socketMessages} from "../../utility/socketConnection.ts";
 
 
 const useLogViewer = () => {
@@ -8,7 +8,7 @@ const useLogViewer = () => {
 	const [loaded, setLoaded] = useState(false)
 	
 	const restorePreviousLogMessages = (user_session: string) => {
-		fetch(API_HTTP_URL + "/get_messages?user_id=" + user_session).then(data => data.json()).then(messages => {
+		apiFetch("/get_messages?user_id=" + user_session).then(data => data.json()).then(messages => {
 			setMessages(messages)
 			setLoaded(true)
 			console.log("Loaded messages")
@@ -20,7 +20,7 @@ const useLogViewer = () => {
 	useEffect(() => {
 		const restoreLog = async () => {
 			if (!user.has_user_session()) {
-				await fetch(API_HTTP_URL + "/get_session").then(response => response.text()).then(session => {
+				await apiFetch("/get_session").then(response => response.text()).then(session => {
 					console.log("Session", session)
 					user.set_user_session(session)
 				})
@@ -32,11 +32,11 @@ const useLogViewer = () => {
 	
 	useEffect(() => {
 		const addMessage = (msg: string) => setMessages(prevMessages => [...prevMessages, msg]);
-		socketConnection.on(socketMessages.button_response_event, addMessage)
+		socketConnection.on(socketMessages.button_event, addMessage)
 		
 		// remove socket handler
 		return () => {
-			socketConnection.off(socketMessages.button_response_event, addMessage)
+			socketConnection.off(socketMessages.button_event, addMessage)
 		}
 	}, [])
 	
